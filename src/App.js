734cb6af4be032cf6c36bef95af9a440eec05997
Danujaya  Liyanage <2021is054@stu.ucsc.cmb.ac.lk>
2023-10-20 09:04:00 +0530

    Design GUI of  application

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './index.css'; 
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [imageUrl, setImageUrl] = useState('');
+
+  const handleImageAnalysis = () => {
+    // Add logic for image analysis (to be implemented in the next exercises)
+    console.log('Image analysis triggered');
+  };
+
+  const handleImageGeneration = () => {
+    // Add logic for image generation (to be implemented in the next exercises)
+    console.log('Image generation triggered');
+  };
+
+  return (
+    <div className="app-container">
+      <h1>Image Analyzer and Generator</h1>
+      <div>
+        <label htmlFor="imageUrl">Enter Image URL:</label>
+        <input
+          type="text"
+          id="imageUrl"
+          value={imageUrl}
+          onChange={(e) => setImageUrl(e.target.value)}
+        />
+      </div>
+      <div>
+        <button onClick={handleImageAnalysis}>Analyze Image</button>
+        <button onClick={handleImageGeneration}>Generate Image</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
